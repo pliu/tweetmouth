a73f5d1280815e92b8c46c64184120dbfe28a4a9
W Pearson <wpear089@uottawa.ca>
2017-02-12 01:45:47 -0500

    Add basic static file web server using Spark

--- src/main/java/ca/xqz/tweetmouth/Main.java
+++ src/main/java/ca/xqz/tweetmouth/Main.java
@@ -17,11 +17,13 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
+import spark.Spark;
+import spark.Route;
 
 class Main {
     private final static int CLIENT_PORT = 9300;
 
-    public static void main(String[] args) {
+    private static TransportClient getTransportClient() {
         InetAddress localhost;
         try {
             localhost = InetAddress.getLocalHost();
@@ -30,8 +32,12 @@ class Main {
             throw new RuntimeException(e);
         }
 
-        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+        return new PreBuiltTransportClient(Settings.EMPTY)
             .addTransportAddress(new InetSocketTransportAddress(localhost, CLIENT_PORT));
+    }
+
+    public static void main(String[] args) {
+        TransportClient client = getTransportClient();
         System.out.println("Successfully created a client");
         try {
             List<Tweet> tweetList = TweetParser.parseTweetsFromFile(System.in);
@@ -40,6 +46,9 @@ class Main {
             System.out.println("IO Exception in parsing tweets");
             throw new RuntimeException(e);
         }
+
+        Spark.staticFileLocation("/public");
+        Spark.init();
 
         client.close();
         System.out.println("Successfully closed a client");
