17c8c039c017e4c966585f0817515da44fcf7a5c
Peng Liu <pengliu12@gmail.com>
2017-02-14 14:51:06 -0500

    Refactoring
    Factored out ES loading from Java server
    Refactored tweet parsing to be more generic

---
+++ src/main/java/ca/xqz/tweetmouth/TweetLoader.java
@@ -0,0 +1,30 @@
+package ca.xqz.tweetmouth;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+
+import static java.lang.System.exit;
+
+public class TweetLoader {
+
+    private final static String HOST = "127.0.0.1";
+    private final static int CLIENT_PORT = 9300;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Takes 1 argument, the path to the file with the tweet data to be parsed and loaded");
+            exit(1);
+        }
+
+        String path = args[0];
+        TweetParser parser = new TweetParser(path);
+
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(HOST), CLIENT_PORT));
+        System.out.println("Successfully created a client");
+    }
+}
--- src/main/java/ca/xqz/tweetmouth/TweetParser.java
+++ src/main/java/ca/xqz/tweetmouth/TweetParser.java
@@ -1,56 +1,81 @@
 package ca.xqz.tweetmouth;
 
-import ca.xqz.tweetmouth.Tweet;
-
 import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TweetParser {
 
-    public static List<Tweet> parseTweetsFromFile(InputStream in) throws IOException {
-        Map<String, Integer> tweetFieldMap = new HashMap<String, Integer>();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(in));
-        List<Tweet> tweets = new ArrayList<Tweet>();
+    private BufferedReader input;
+    private List<String> orderedLabels;
 
-        String line = bf.readLine();
-        if (line == null) {
-            return tweets;
+    public TweetParser(String path) throws Exception {
+        this.input = new BufferedReader(new FileReader(path));
+        orderedLabels = processTweet(input.readLine());
+    }
+
+    public TweetParser(List<String> orderedLabels) {
+        this.orderedLabels = orderedLabels;
+    }
+
+    public List<Map<String, String>> getMappedTweetsFromFile(int num) throws Exception {
+        if (input == null) {
+            throw new RuntimeException("Trying to get tweets from a non-existent file");
         }
-
-        String[] cols = line.split("~", -1);
-        for (int i = 0; i < cols.length; i++) {
-            tweetFieldMap.put(cols[i], i);
-        }
-
-        for (line = bf.readLine(); line != null; line = bf.readLine()) {
-            String[] tokens = line.split("~", -1);
-            while (tokens.length < 8) {
-                line += "\n";
-                String tmp = bf.readLine();
-                if (tmp == null)
-                    return tweets;
-                line += tmp;
-                tokens = line.split("~", -1);
+        ArrayList<Map<String, String>> mappedTweets = new ArrayList<>();
+        String line = input.readLine();
+        for (int i = 0; line != null && i < num; i ++) {
+            while (line != null) {
+                Map<String, String> tweetMap = getTweetMap(line);
+                if (tweetMap != null) {
+                    mappedTweets.add(tweetMap);
+                    break;
+                }
+                line = input.readLine();
             }
-
-            String id = tokens[tweetFieldMap.get("Tweet Id")];
-            String handle = tokens[tweetFieldMap.get("User Name ")];
-            String name = tokens[tweetFieldMap.get(" User ID")];
-            String msg = tokens[tweetFieldMap.get("Tweet message")];
-            String creation = tokens[tweetFieldMap.get("creation time")];
-            String user_loc = tokens[tweetFieldMap.get("User location")];
-            String geo_loc = tokens[tweetFieldMap.get("GeoLocation")];
-            String user_id = tokens[tweetFieldMap.get("getUser ID")];
-            tweets.add(new Tweet(id, handle, name, msg, creation, user_loc, geo_loc, user_id));
+            line = input.readLine();
         }
+        return mappedTweets;
+    }
 
-        return tweets;
+    private Map<String, String> getTweetMap(String tweet) {
+        List<String> tokens = processTweet(tweet);
+        if (tokens.size() != orderedLabels.size()) {
+            System.out.println(tokens.get(0) + " is not properly formatted");
+            return null;
+        }
+        HashMap<String, String> tweetMap = new HashMap<>();
+        for (int i = 0; i < orderedLabels.size(); i ++) {
+            tweetMap.put(orderedLabels.get(i), tokens.get(i));
+        }
+        return tweetMap;
+    }
+
+    private List<String> processTweet(String tweet) {
+        List<String> tokenList = Arrays.asList(tweet.split("~"));
+        List<String> processedTokens = tokenList.stream().map(s -> s.trim()).collect(Collectors.toList());
+        return processedTokens;
+    }
+
+    @Override
+    public void finalize() throws Exception {
+        if (input != null) {
+            input.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TweetParser parser = new TweetParser("D:\\Programming\\tweetmouth\\TweetsDataset.txt");
+        for (String label : parser.orderedLabels) {
+            System.out.println(label);
+        }
+        List<Map<String, String>> mappedTweets = parser.getMappedTweetsFromFile(100);
+        for (Map<String, String> map : mappedTweets) {
+            for (String key : map.keySet()) {
+                System.out.print(key + ": " + map.get(key) + "\t");
+            }
+            System.out.println();
+        }
     }
 }
