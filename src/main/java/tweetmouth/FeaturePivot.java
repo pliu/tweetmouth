758b37bb656b287ea4f220deec154d3a3099bca3
Peng Liu <pengliu12@gmail.com>
2017-04-21 01:35:44 -0400

    Refactor
    Condensed the logic into more concrete modules

--- src/main/java/tweetmouth/FeaturePivot.java
+++ src/main/java/tweetmouth/FeaturePivot.java
@@ -1,6 +1,17 @@
 package tweetmouth;
 
+import org.apache.spark.api.java.JavaPairRDD;
+
 public class FeaturePivot {
 
+    // Generate bi-gram and tri-gram features from cleaned tweets
+    // Treat hashtags as individual features
+    // Aggregate features
+    public static JavaPairRDD<Long, String> cleanAndFilterTweets(JavaPairRDD<Long, String> tweets) {
+        return tweets;
+    }
 
+    public static JavaPairRDD<Long, String> generateFeatureVectors(JavaPairRDD<Long, String> tweets) {
+        return tweets;
+    }
 }
