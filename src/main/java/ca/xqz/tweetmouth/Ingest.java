0de9439de09aa444cc472589353ced18651eb68c
W Pearson <wpear089@uottawa.ca>
2017-04-30 22:55:08 -0400

    Ingest 100 tweets at a time in a loop

--- src/main/java/ca/xqz/tweetmouth/Ingest.java
+++ src/main/java/ca/xqz/tweetmouth/Ingest.java
@@ -12,19 +12,19 @@ import java.util.List;
 import java.util.stream.Stream;
 
 class Ingest {
+    private static int PARSED_TWEET_BUF_SIZE = 100;
 
-    public static void main(String[] args) {
-        TweetParser parser = new TweetParser();
-        ESClient client = new ESClient();
-        final Pipeline pipeline = Pipeline.getPipeline();
-
+    private static boolean ingestTweets(TweetParser parser, Pipeline pipeline, ESClient client) {
         List<Tweet> tweets;
+        boolean moreTweets;
+
         try {
-            tweets = parser.getParsedTweets(10000);
+            tweets = parser.getParsedTweets(PARSED_TWEET_BUF_SIZE);
         } catch (IOException e) {
             System.out.println("IO Exception in parsing tweets");
             throw new RuntimeException(e);
         }
+        moreTweets = tweets.size() == PARSED_TWEET_BUF_SIZE;
 
         Stream<Tweet> annotations = tweets.parallelStream().map(
             tweet -> {
@@ -37,6 +37,15 @@ class Ingest {
             });
 
         client.loadTweets(annotations);
+        return moreTweets;
+    }
+
+    public static void main(String[] args) {
+        TweetParser parser = new TweetParser();
+        ESClient client = new ESClient();
+        final Pipeline pipeline = Pipeline.getPipeline();
+
+        while (ingestTweets(parser, pipeline, client));
         client.close();
     }
 }
