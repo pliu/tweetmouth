4a5cdc8fd8b813dfb9e3cd98ba86b1d029a4d234
W Pearson <wpear089@uottawa.ca>
2017-05-01 03:30:18 -0400

    Embed Tweets in Results page
    Use fuzzy query against elastic search to generate results.
    Use twitter API to embed result tweets in results page.

--- src/main/java/ca/xqz/tweetmouth/WebServer.java
+++ src/main/java/ca/xqz/tweetmouth/WebServer.java
@@ -1,8 +1,11 @@
 package ca.xqz.tweetmouth;
 
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.search.SearchHit;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import spark.ModelAndView;
@@ -17,18 +20,27 @@ class WebServer {
 
     private final static TemplateEngine TEMPLATE = new MustacheTemplateEngine();
     private final static TemplateViewRoute SEARCH = (req, res) -> {
-        SearchResponse search_res = client.simpleQuerySearch(req.queryParams("q"));
+        SearchResponse search_res = client.query(req.queryParams("q"));
         Map<String, Object> temp_res = new HashMap<String, Object>();
+        List<String> hits = new ArrayList<String>();
+        for (SearchHit hit : search_res.getHits()) {
+            Map m = hit.sourceAsMap();
+            if (m == null)
+                continue;
+            hits.add("{\"id\":\"" + m.get("id") + "\", \"handle\":\"" + m.get("handle") + "\"}");
+        }
         temp_res.put("resp", search_res);
-        temp_res.put("hits", search_res.getHits());
+        temp_res.put("hits", hits);
         temp_res.put("total_hits", search_res.getHits().getTotalHits());
         temp_res.put("suggest", search_res.getSuggest());
-        System.out.println(search_res);
         return new ModelAndView(temp_res, "results.html");
     };
 
     public static void main(String[] args) {
         Spark.staticFileLocation("/public");
+        Spark.before((req, resp) -> {
+                resp.header("Access-Control-Allow-Origin", "*");
+            });
         Spark.get("/search", SEARCH, TEMPLATE);
     }
 
