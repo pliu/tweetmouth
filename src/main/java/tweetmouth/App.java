0bcdd896e434ff1af768b3bb2ce0c6c9188e88ba
Peng Liu <pengliu12@gmail.com>
2017-05-02 16:59:23 -0400

    Clustering
    Added a method to find k
    Added clustering

--- src/main/java/tweetmouth/App.java
+++ src/main/java/tweetmouth/App.java
@@ -20,33 +20,47 @@ public class App {
 
         JavaPairRDD<Long, TweetElements> filteredTweets = TweetPivot.parseAndFilterTweets(validTweets, true,
                 true);
-        long numTweets = filteredTweets.count();
 
-        JavaPairRDD<String, Integer> features = TweetPivot.parseAndFilterFeatures(filteredTweets, true, true,
-                numTweets);
-
+        boolean featuresCached = true;
         boolean featureVectorsCached = true;
+
+        long numTweets = 0;
+        if (!featuresCached || !featureVectorsCached) {
+            numTweets = filteredTweets.count();
+        }
+
+        JavaPairRDD<String, Integer> features = TweetPivot.parseAndFilterFeatures(filteredTweets, numTweets,
+                featuresCached, true);
+
         Map<String, Integer> enumeratedFeatures = null;
         Map<String, Integer> documentFeatureCounts = null;
         if (!featureVectorsCached) {
-            features = features.cache();
-            enumeratedFeatures = Utils.enumerate(features.map(tuple -> tuple._1()).collect());
+            enumeratedFeatures = Utils.enumerate(features.map(Tuple2::_1).collect());
             documentFeatureCounts = Utils.getMap(features.collect());
         }
 
         JavaPairRDD<Long, Vector> featureVectors = TweetPivot.generateFeatureVectors(filteredTweets,
                 enumeratedFeatures, documentFeatureCounts, numTweets, featureVectorsCached, true);
 
-        // Clustering.cluster(featureVectors, 5);
+        JavaPairRDD<Integer, Iterable<Long>> clusters = Clustering.cluster(featureVectors);
 
         // System.out.println(validTweets.count());
         // System.out.println(filteredTweets.count());
         // System.out.println(features.count());
         // System.out.println(featureVectors.count());
-
-        for (Tuple2<String, Integer> t : features.takeSample(false, 100)) {
-            System.out.println(t._1() + ": " + t._2());
+        Map<Integer, Iterable<Long>> clusterMap = Utils.getMap(clusters.collect());
+        for (Map.Entry<Integer, Iterable<Long>> e : clusterMap.entrySet()) {
+            System.out.print(e.getKey() + ": ");
+            int count = 0;
+            for (Long l : e.getValue()) {
+                count++;
+            }
+            System.out.println(count);
         }
+
+        /*for (Tuple2<String, Integer> t : features.takeSample(false, 100)) {
+            System.out.println(t._1() + ": " + t._2());
+        }*/
 
         /*for (Tuple2<Long, Vector> t : featureVectors.takeSample(false, 10)) {
             System.out.println(t._1() + ": " + t._2().toJson());
--- src/main/java/tweetmouth/Clustering.java
+++ src/main/java/tweetmouth/Clustering.java
@@ -5,21 +5,52 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.clustering.BisectingKMeans;
 import org.apache.spark.mllib.clustering.BisectingKMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
+import scala.Tuple2;
 
+import java.util.function.BiFunction;
+
+// TODO: Currently a bug in mllib of Spark that causes clustering to fail with certain values of k (I think it's any k
+// above 7)
 public class Clustering {
 
-    public static final void findK(JavaPairRDD<Long, Vector> labelledVectors) {
-        JavaRDD<Vector> vectors = labelledVectors.map(tuple -> tuple._2()).cache();
+    private final static BiFunction<Double, Integer, Double> COST_FUNC = (cost, numClusters) -> cost + numClusters*25;
+    private final static int STARTING_K = 5;
 
+    public static JavaPairRDD<Integer, Iterable<Long>> cluster(JavaPairRDD<Long, Vector> labelledVectors) {
+        JavaRDD<Vector> vectors = labelledVectors.map(Tuple2::_2).cache();
+        BisectingKMeansModel model = optimizeModel(vectors);
+        return assignToClusters(labelledVectors, model);
     }
 
-    public static final void cluster(JavaPairRDD<Long, Vector> vectors, int clusters) {
-        BisectingKMeans bkm = new BisectingKMeans().setK(clusters);
-        JavaRDD<Vector> v = vectors.map(tuple -> tuple._2());
-        BisectingKMeansModel model = bkm.run(v);
-        System.out.println(model.computeCost(v));
-        for (Vector vec : model.clusterCenters()) {
-            System.out.println(vec);
+    private static JavaPairRDD<Integer, Iterable<Long>> assignToClusters(JavaPairRDD<Long, Vector> labelledVectors,
+                                                                          BisectingKMeansModel model) {
+        return labelledVectors
+                .mapToPair(tuple -> new Tuple2<>(model.predict(tuple._2()), tuple._1()))
+                .groupByKey();
+    }
+
+    private static BisectingKMeansModel optimizeModel(JavaRDD<Vector> vectors) {
+        int k = STARTING_K;
+        double lowestCost = Double.MAX_VALUE;
+        BisectingKMeansModel bestModel = null;
+        while(true) {
+            BisectingKMeansModel model = getModel(vectors, k);
+            double cost = COST_FUNC.apply(model.computeCost(vectors), k);
+            System.out.println(cost);
+            if (cost < lowestCost) {
+                lowestCost = cost;
+                bestModel = model;
+            } else {
+                 break;
+            }
+            k++;
         }
+        return bestModel;
+    }
+
+    private static BisectingKMeansModel getModel(JavaRDD<Vector> vectors, int clusters) {
+        BisectingKMeans bkm = new BisectingKMeans().setK(clusters);
+        BisectingKMeansModel model = bkm.run(vectors);
+        return model;
     }
 }
